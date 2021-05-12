f064021be9468edc0f8632375f34b6d5061a10b7
samgabrail <sameh.gabrail@gmail.com>
2021-05-12 19:00:04 -0400

    clean-up outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-// output "endpoints" {
-//   value = <<EOF
-
-//       for instance in aws_instance.vault-server:
-//         instance.name => "public: ${instance.public_ip} | private: ${instance.private_ip}"
-//         "$ ssh -l ubuntu ${instance.public_ip} -i ${var.key_name}.pem"
-
- 
-// EOF
-// }
-
-
 output "endpoints" {
   value = {
 
